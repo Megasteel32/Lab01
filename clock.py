4278cd02fac7cbfc6cbd1336fe9e0f2d7c6233a1
Megasteel32 <megasteel32@gmail.com>
2020-09-13 22:55:52 -0500

    Initial

---
+++ clock.py
@@ -0,0 +1,26 @@
+# By submitting this assignment, I agree to the following:
+#  "Aggies do not lie, cheat, or steal, or tolerate those who do"
+#  "I have not given or received any unauthorized aid on this assignment"
+#
+# Name:        Luca Maddaleni, 330001030
+# Section:     273
+# Assignment:  Unreadable Clock
+# Date:        8/25/2020
+
+import numpy
+import matplotlib
+
+from math import *
+
+print(cos(0)/cos(0))
+print(10%3+10%3)
+print(1000//325)
+print(fabs(2**2))
+print(sqrt(sqrt(5**4)))
+print(exp(0)*36-(3*2*5+log10(1)))
+print((7**7)/(7**6))
+print(round(sin(radians(45)))*2**3)
+print((3**3)/3)
+print(round(tan(radians(45)))*10)
+print(34587349857+9498484482-44085834328)
+print(1+1+1+1+1+1+1+1+1+1+1+0+1)
